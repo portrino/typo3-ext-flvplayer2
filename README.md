286208990a30f6258d2735f693d4014eddd5fbd8
Jose Antonio Guerra <jaguerra@icti.es>
2014-01-20 17:32:17 +0100

    [TASK] Updated docs

---
+++ README.md
@@ -0,0 +1,8 @@
+# TYPO3 CMS extension "flvplayer2"
+
+This Video Player allows you to show your MP4 or FLV videos to a broader audience as with Quicktime, Windows Media or Real Media. This extension is based and backwards-compatible with extension flvplayer, adding standards compliance, an updated player (JW Player or Flowplayer) and improved support for YouTube and Vimeo.
+
+*Project home:* http://forge.typo3.org/projects/extension-flvplayer2
+*GIT repo:* https://github.com/icti/typo3-ext-flvplayer2
+
+[![Build Status](https://travis-ci.org/icti/typo3-ext-flvplayer2.png?branch=1.4)](https://travis-ci.org/icti/typo3-ext-flvplayer2)
--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Flash Video Player v2 (FLV and MP4)',
-	'description' => 'This Video Player allows you to show your MP4 or FLV videos to a broader audience as with Quicktime, Windows Media or Real Media. This extension is based and backwards-compatible with extension flvplayer, adding standards compliance and an updated player (JW Player or Flowplayer).',
+	'title' => 'Flash Video Player v2 (FLV, MP4, Youtube and Vimeo)',
+	'description' => 'This Video Player allows you to show your MP4 or FLV videos to a broader audience as with Quicktime, Windows Media or Real Media. This extension is based and backwards-compatible with extension flvplayer, adding standards compliance, an updated player (JW Player or Flowplayer) and improved support for YouTube and Vimeo.',
 	'category' => 'plugin',
 	'shy' => 0,
 	'version' => '1.4.0-dev',
